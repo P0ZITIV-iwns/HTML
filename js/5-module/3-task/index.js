8dfb6155a74f50634cb88c20fe4f62728fe8d47d
P0ZITIV <66378765+P0ZITIV-iwns@users.noreply.github.com>
2023-03-11 00:59:44 +0500

    Update index.js

--- js/5-module/3-task/index.js
+++ js/5-module/3-task/index.js
@@ -1,7 +1,7 @@
 function initCarousel() {
   let translate = 0;
   document.querySelector(".carousel__arrow_left").style.display = "none";
-  document.addEventListener("click", (event) => {
+  spin = (event) => {
     if (event.target.closest(".carousel__arrow_right")){
       translate -= document.querySelector(".carousel__inner").offsetWidth;
     }
@@ -9,16 +9,18 @@ function initCarousel() {
       translate += document.querySelector(".carousel__inner").offsetWidth;
     }
     
-    document.querySelector(".carousel__inner").style.transform = 'translateX(' + translate + 'px)';
+    document.querySelector(".carousel__inner").style.transform = `translateX(${translate}px)`;
 
     if (translate === document.querySelector(".carousel__inner").offsetWidth * -3){
       document.querySelector(".carousel__arrow_right").style.display = "none";
     }
-    else if (translate === 0){
+    else if (!translate){
       document.querySelector(".carousel__arrow_left").style.display = "none";
     } else {
       document.querySelector(".carousel__arrow_left").style.display = '';
       document.querySelector(".carousel__arrow_right").style.display = '';
     }
-  })
+  }
+
+  document.addEventListener("click", spin)
 }

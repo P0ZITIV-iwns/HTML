f744c3b03678c4109aa7c1a2328a28bda39495af
P0ZITIV <66378765+P0ZITIV-iwns@users.noreply.github.com>
2023-03-11 00:53:38 +0500

    Update index.js

--- js/5-module/2-task/index.js
+++ js/5-module/2-task/index.js
@@ -1,6 +1,5 @@
 function toggleText() {
   document.querySelector(".toggle-text-button").onclick = function(){
-    let text = document.getElementById("text");
     text.hidden = !text.hidden;
   }
 }
